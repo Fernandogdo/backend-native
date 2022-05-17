2ff87e95a52b464aee77ba3872e5ea309d76d174
fernandogdo <fernandojaviergdo@gmail.com>
2022-05-16 23:25:01 -0500

    cambios

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,22 +1,22 @@
-import {Request, Response} from 'express'
-import  Category  from '../models/Category'
-import path  from 'path'; 
+import { Request, Response } from 'express'
+import Category from '../models/Category'
+import path from 'path';
 import fs from 'fs-extra';
 
-export async function getCategories(req:Request, res:Response): Promise<Response>{
+export async function getCategories(req: Request, res: Response): Promise<Response> {
     const categories = await Category.find();
     return res.json(categories)
 }
 
-export async function getCategory(req:Request, res:Response): Promise<Response>{
+export async function getCategory(req: Request, res: Response): Promise<Response> {
     const { id } = req.params
     const category = await Category.findById(id)
     // console.log(req.params.id)
     return res.json(category)
 }
 
-export async function createCategory(req:Request, res:Response): Promise<Response>{
-    const { title, description} = req.body
+export async function createCategory(req: Request, res: Response): Promise<Response> {
+    const { title, description } = req.body
     const newPhoto = {
         title: title,
         description: description,
@@ -33,11 +33,15 @@ export async function createCategory(req:Request, res:Response): Promise<Respons
 }
 
 
-export async function deleteCategory(req: Request, res: Response): Promise<Response>{
+export async function deleteCategory(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
     const category = await Category.findByIdAndRemove(id);
-    if (category){
-        fs.unlink(path.resolve(category.imagePath))
+
+    if (category) {
+        fs.unlink(path.resolve(category.imagePath), function (err) {
+            if (err) throw err;
+
+        })
     }
     return res.json({
         message: "Category deleted",
@@ -48,12 +52,12 @@ export async function deleteCategory(req: Request, res: Response): Promise<Respo
 
 export async function updatedCategory(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
-    const {title, description } = req.body;
+    const { title, description } = req.body;
     console.log(req.body)
     const updatedCategory = await Category.findByIdAndUpdate(id, {
         title,
         description
-    }, {new: true});
+    }, { new: true });
 
     return res.json({
         message: "Update susccesfully",
